0a56e7901f3a7ddbcc5235b99453226a08773c0c
Aditya Shankar <aditya@Adityas-MacBook-Air.local>
2026-08-02 01:06:23 +0530

    Initial project setup

---
+++ src/main/java/OpsLens/OpslensApplication.java
@@ -0,0 +1,13 @@
+package OpsLens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpslensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpslensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/OpsLens/OpslensApplicationTests.java
@@ -0,0 +1,13 @@
+package OpsLens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpslensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
